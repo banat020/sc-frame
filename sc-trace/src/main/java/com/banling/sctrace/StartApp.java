bc60e415f1c78fddb553efe6762213325476fb9b
banat020 <lingat020@sina.com>
2018-08-09 13:57:19 +0800

    zipkin collect trace and then save to mysql

--- sc-trace/src/main/java/com/banling/sctrace/StartApp.java
+++ sc-trace/src/main/java/com/banling/sctrace/StartApp.java
@@ -2,6 +2,7 @@ package com.banling.sctrace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import zipkin.server.EnableZipkinServer;
 
@@ -11,4 +12,9 @@ public class StartApp {
 	public static void main(String[] args) {
 		SpringApplication.run(StartApp.class, args);
 	}
+	
+//	@Bean
+//    public MySQLStorage mySQLStorage(DataSource datasource) {
+//        return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
+//    }
 }
